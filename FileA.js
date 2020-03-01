92d015c765ed3dc0ae7b8490f00ac99ecce33e40
Owais Ibrahim <owaisibrahim99@gmail.com>
2020-03-01 13:19:59 +0500

    Files added

---
+++ FileA.js
@@ -0,0 +1,5 @@
+const abc = () => {
+    console.log('This is a File A')
+}
+
+console.log('Some content added');
---
+++ FileB.js
@@ -0,0 +1,6 @@
+let age = 20;
+printAge(age);
+
+function printAge(age) {
+    console.log(age);
+}
